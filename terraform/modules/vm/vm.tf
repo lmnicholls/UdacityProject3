b9a3db20c00ccbb74eee7f2607b3648367f1a290
Lauren Nicholls <lnicholls@relias.com>
2023-08-03 12:28:16 -0400

    update terraform files

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -16,20 +16,15 @@ resource "azurerm_linux_virtual_machine" "" {
   location            = "East US"
   resource_group_name = "Azuredevops"
   size                = "Standard_DS2_v2"
-  admin_username      = "azureuser"
+  admin_username      = "adminuser"
   network_interface_ids = []
   admin_ssh_key {
-    username   = "azureuser"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCZif1eu+iHUu0uuHyZo0bLPn+af7s+CM7uYa9zAuivDQNh6+hIPRP6EcEOBBF9BI6JwtlGRyHL+LE8VTzhKa47ONuTAKxhD6XK9xPhQU9qmK0xKdnvG9/7gHq3+gHHbXos3w0tuGyP0M3vJw1FdPcvyTEFxAPGhNKJqTisyyPAQZAQiKf75JU4G6+O0ytgXCgzDcApE5+RzTbv6Tw2xDxWTc2keOre0QrQqf5oIpzFB8ER4iCqwLGIEqg07zy1Wb4dKyzLtl+S1usno3OJpOIT5LdVzlksJA5WgvCS/+ZvxEgr5c+NYWjKzSS/ZV2YqIyPfw4YymQhZmPR6L5zdZX8i6CmcX5yG8i0Srx2un0I1Hm6KAfqzIZVDt/FEfUCKqkjrqLm6f66c4nK2/APgpn4NbZtVn28d6tC8E+enImDwfMJGJUWaS1UIT9xSvgAPYdEabz/QM36TJAg3W3UeF7QzyDcIygfjUr7iKQpdi+pmyDsmGFo34opqD52SUtLt/k= odl_user@cc-ec005490-6d9fc4b856-m4vk7"
+    username   = "adminuser"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQD2jPrC7YjBW/GN0iLDrgv/UXsam9JwnNtzDIjHaxCxsXiVCCAcRQ6KoFzndMxjUKb0GjgwAsbMmoKcoJQpbppPi/FDnXEYxQ980RKEPGEVVSbEnoHoG8MvjC5CNFkeeENQkaL3ybo2yAOI6cHCQNUbS3ZYuwkZr4tJwgScZPTFnosKUke6ekc8yYctQyUSQHedV3ayLkQP+o+xgNBrpqxXuq/P0fVXyvlRc4DEe/e2hrkfuliXmmoyVJYprEyfXL8KjJwL42LHq+3P5su0nqv4d7h1dAlGt/xnRWgXhgbsdwy7FEEBb9L/jsB15dQlMZWQPpbaFZKpAyrsJbNbDQY1pt+wKX5+tmzznhJLp0TA9MiSJ4g2ZoAQl10PwiVlK94zS+VnCANhD30AILYrhGfQfBP6kzxxzXlSad2EJO0SpGuJNl9KvQvxQBSU0IodEi2vyesewmlQKILpoHOVoBYXpxN++xbKEM+U0+7MVgJZCc7gWMhF02CzGHkkIIKkYQk= relias\lnicholls@RELIAS-R90VR4HC"
   }
   os_disk {
     caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+  source_image_id = "VMImage"
 }
