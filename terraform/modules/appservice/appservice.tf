904b81ef0f525393ac82e431c69f1a5d7502317a
Lauren Nicholls <lnicholls@relias.com>
2023-08-03 15:57:27 -0400

    update azure piplines yaml

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -4,6 +4,11 @@ resource "azurerm_service_plan" "test" {
   resource_group_name = "${var.resource_group}"
   os_type             = "Windows"
   sku_name            = "F1"
+
+    application_stack {
+    current_stack       = "dotnet"
+    dotnet_version      = "v6.0"
+  }
 }
 
 resource "azurerm_windows_web_app" "test" {
@@ -17,10 +22,5 @@ resource "azurerm_windows_web_app" "test" {
   }
   site_config {
     always_on = false
-  }
-
-  application_stack {
-    current_stack       = "dotnet"
-    dotnet_version      = "v6.0"
   }
 }
